ef42b81a3deaf5e8c8b108146ea1f17c63effb5d
lrlt11 <lunaralet@hotmail.be>
2025-04-14 16:33:42 +0200

    website -milestone2

--- Milestones/milestone 2/website/js/main.js
+++ Milestones/milestone 2/website/js/main.js
@@ -1,28 +1,25 @@
-console.log("main.js is connected!");
-// Later: code for your charts or visual widgets.
 function goToChart(chartNumber) {
-    // Get both chart sections
-    const chart1 = document.getElementById('chart1');
-    const chart2 = document.getElementById('chart2');
+
+    const chart1 = document.getElementById('podium');
+    const chart2 = document.getElementById('over_the_years');
     
-    // Hide both first
     chart1.style.display = 'none';
     chart2.style.display = 'none';
   
-    // Show the chosen one
     if (chartNumber === 1) {
       chart1.style.display = 'block';
     } else {
       chart2.style.display = 'block';
     }
   
-    // Update which nav dot is active
     const dots = document.querySelectorAll('.dot');
     dots.forEach(dot => dot.classList.remove('active'));
+    console.log(dots);
     if (chartNumber === 1) {
       dots[0].classList.add('active');
+
     } else {
-      dots[1].classList.add('active');
+      dots[3].classList.add('active');
     }
   }
   
